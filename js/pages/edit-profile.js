db36defc39a5c05daad964e7727e49b1d9929ab2
wanawsha <wanawshaak@gmail.com>
2025-12-07 16:01:38 +0100

    Added the code to edit-profile.js

---
+++ js/pages/edit-profile.js
@@ -0,0 +1,57 @@
+import { getStoredUser, storeUser, storeCredits } from "../utils/storage.js";
+import { setupNavbar } from "../utils/navbar.js";
+import { authHeaders } from "../utils/api.js";
+
+setupNavbar();
+
+const form = document.getElementById("edit-profile-form");
+const avatarInput = document.getElementById("avatar-url");
+const bannerInput = document.getElementById("banner-url");
+const bioInput = document.getElementById("bio");
+
+const user = getStoredUser();
+if (!user) {
+    window.location.href = ".login.html";
+}
+
+avatarInput.value = user.avatar || "";
+bannerInput.value = user.banner || "";
+bioInput.value = user.bio || "";
+
+async function updateProfile(e) {
+    e.preventDefault();
+
+    const updatedProfile = {
+        avatar: avatarInput.value.trim(),
+        banner: bannerInput.value.trim(),
+        bio: bioInput.value.trim(),
+    };
+
+    try {
+        const response = await fetch(
+            `https://v2.api.noroff.dev/auction/profiles/${user.name}`,
+            {
+                method: "PUT",
+                headers: authHeaders(),
+                body: JSON.stringify(updatedProfile),
+            }
+        );
+
+        const result = await response.json();
+
+        if (!response.ok) {
+            throw new Error(result.errors?.[0]?.message || "Update failed");
+        }
+
+        storeUser(result.data);
+        storeCredits(result.data.credits);
+
+        alert("Profile updated successfully!");
+
+        window.location.href = "./profile.html";
+    } catch (error) {
+        alert("Error: " + error.message);
+    }
+}
+
+form.addEventListener("submit", updateProfile);
